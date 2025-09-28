8e517ef559f950fb7cf73b2e71372068f09439e4
MrDataPsycho <mr.data.psycho@gmail.com>
2025-09-28 13:54:21 +0200

    removed dimention from vector store interface.

--- src/contramate/dbs/interfaces/vector_store.py
+++ src/contramate/dbs/interfaces/vector_store.py
@@ -4,12 +4,6 @@ from abc import ABC, abstractmethod
 
 # Abstract base class for vector databases
 class VectorDBABC(ABC):
-    @property
-    @abstractmethod
-    def dimension(self) -> int:
-        """Property to get the dimension of the vectors used in the database."""
-        pass
-
     @abstractmethod
     def save(self) -> None:
         """Save the database to the specified path."""
@@ -21,7 +15,7 @@ class VectorDBABC(ABC):
         pass
 
     @abstractmethod
-    def search(self, query: str, top_k: int = 20) -> List[Any]:
+    def search(self, query: str, top_k: int = 20, filters: dict[str, Any] = None) -> List[Any]:
         """Search for the top_k most similar vectors to the query_vector."""
         pass
 
